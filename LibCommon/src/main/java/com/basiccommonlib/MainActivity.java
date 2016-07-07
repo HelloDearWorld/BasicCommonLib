f119d3f2a9d8124c0f102a82703df19add0b0a37
ruanyaguang <ruanyaguang@kobox.tv>
2016-07-07 14:11:16 +0800

    delete ButterKnife

--- LibCommon/src/main/java/com/basiccommonlib/MainActivity.java
+++ LibCommon/src/main/java/com/basiccommonlib/MainActivity.java
@@ -5,15 +5,12 @@ import android.os.Bundle;
 
 import com.basiccommonlib.activity.BaseCommonActivity;
 
-import butterknife.ButterKnife;
-
 public class MainActivity extends BaseCommonActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
     }
 
     @Override
