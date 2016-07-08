08a4e294a5cfd4225f98dc8356a1167e0c7145c5
ruanyaguang <ruanyaguang@kobox.tv>
2016-07-08 16:20:56 +0800

    add CommonViewpager

---
+++ LibCommon/src/main/java/com/basiccommonlib/weight/CommonViewpager.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (C) © 2016
+ * BasicCommonLib
+ * SuperViewpager.java
+ */
+package com.basiccommonlib.weight;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.RelativeLayout;
+
+import com.basiccommonlib.R;
+
+/**
+ * @author
+ * @since 16/7/8 上午11:04
+ * @version 1.0
+ *          <p>
+ *          <strong>Features draft description.主要功能介绍</strong>
+ *          </p>
+ */
+public class CommonViewpager extends RelativeLayout {
+
+    private Context context;
+
+    public CommonViewpager(Context context) {
+        super(context);
+        initView(context);
+    }
+
+    public CommonViewpager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context);
+    }
+
+    public CommonViewpager(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(context);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CommonViewpager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        initView(context);
+    }
+
+    private void initView(Context context) {
+
+        this.context = context;
+        LayoutInflater.from(getContext()).inflate(R.layout.common_superviewpager, this);
+    }
+
+}
